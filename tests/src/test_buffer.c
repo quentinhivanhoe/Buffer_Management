b7baff36751171a3ae4f1c2dcab2036be9f0cb86
Quentin Hivanhoe <quentin.hivanhoe@epitech.eu>
2025-07-10 15:38:25 +0400

    test: add more unti test

--- tests/src/test_buffer.c
+++ tests/src/test_buffer.c
@@ -13,8 +13,17 @@
 
 const char valid_buf[] = "bonjour les amis";
 const size_t valid_size = 17;
+circle_buff_t *list = NULL;
 
-Test(buffer, list_null)
+Test(buffer, invalid_parameters)
 {
     cr_assert(add_buff_node(NULL, valid_buf, valid_size) == false);
+    cr_assert(add_buff_node(&list, NULL, valid_size) == false);
+    cr_assert(add_buff_node(NULL, valid_buf, 400) == false);
+}
+
+Test(buffer, add_a_simple_node)
+{
+    add_buff_node(&list, valid_buf, valid_size);
+    cr_assert_eq(list->buffer, valid_buf);
 }
